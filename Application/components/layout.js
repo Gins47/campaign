92ec8b3e383c5e6f63898e3319b325060f3cac42
Gins <ginspcyriac1993@gmail.com>
2019-05-25 23:58:58 +0530

    Added Header

--- Application/components/layout.js
+++ Application/components/layout.js
@@ -1,11 +1,10 @@
 import React from 'react'
-
+import Header from './header'
 export default (props) =>{
     return(
         <div>
-            <h1> This is Header</h1> 
+            <Header />
                 {props.children}
-            <h1>This is footer</h1>
         </div>
     )
 }
